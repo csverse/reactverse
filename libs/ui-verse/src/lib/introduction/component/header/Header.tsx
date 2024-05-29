7de92d583b2fb99e8d173a793e28b7201cf7368f
neelra8n <neelratn.official@gmail.com>
2024-05-30 01:23:37 +0530

    update introduction

--- libs/ui-verse/src/lib/introduction/component/header/Header.tsx
+++ libs/ui-verse/src/lib/introduction/component/header/Header.tsx
@@ -2,15 +2,19 @@ import styles from './header.module.scss';
 import profile_photo from '../../../../assets/images/profile_photo_no_bg.png';
 
 export const Header = () => {
-  return <div className={styles['header']}>
-    <div className={styles['title-container']}>
-      <h1>Neel Ratn</h1>
-      <h2>Frontend Developer & Creativity Lover</h2>
+  return (
+    <div className={styles['header']}>
+      <div className={styles['title-container']}>
+        <span data-title="heading">Hi <em>I'm </em>Neel Ratn</span>
+        <span data-sub-title="sub-heading">I am a Software Engineer specializing in architecting, and developing end-to-end frontend solutions</span>
+      </div>
+      <div className={styles['profile_photo_container']}>
+        <div className={styles['img_container']}>
+          <img className={styles['profile_photo']} alt='profile_photo' src={profile_photo} />
+        </div>
+      </div>
     </div>
-    <div className={styles['profile_photo_container']}>
-      <img className={styles['profile_photo']} alt='profile_photo' src={profile_photo} />
-    </div>
-  </div>;
+  );
 };
 
 export default Header;
