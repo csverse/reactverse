d3aeb995a58e17a6cde03193a225796dde5945c3
neelra8n <neelratn.official@gmail.com>
2024-05-31 02:14:18 +0530

    updted logo

--- libs/ui-verse/.storybook/theme.ts
+++ libs/ui-verse/.storybook/theme.ts
@@ -1,8 +1,5 @@
 import { create } from "@storybook/theming";
-import logo from './../src/assets/images/ui-verse.png';
 
 export default create({
   base: 'dark',
-  brandImage: logo,
-  brandTitle: 'ui-verse'
 })
