c72f01420324f59fce7ba4160b4b563543b579d2
neelra8n <neelratn.official@gmail.com>
2024-08-23 15:48:26 +0530

    #16 -- new app - clock-control

---
+++ apps/clock-control/src/app/App.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './App.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="ClockControl" />
+    </div>
+  );
+}
+
+export default App;
